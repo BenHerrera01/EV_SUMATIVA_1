78e27f7e6f0cb71f1a6a26b07ed79a1ad70a8d26
Benjamin Herrera <benja.ihs@gmail.com>
2021-11-24 19:02:24 -0300

    data file added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO usuarios(nombre,apellido,email,edad) values("benjamin","herrera","benja@mail.com",24)
+INSERT INTO usuarios(nombre,apellido,email,edad) values("juan","lopez","juan@mail.com",30)
+INSERT INTO usuarios(nombre,apellido,email,edad) values("gabriel","boric","gabriel@mail.com",35)
+
+INSERT INTO productos(nombre,precio,descripcion,cantidad) values("teclado","19990","teclado genius",2)
+INSERT INTO productos(nombre,precio,descripcion,cantidad) values("mouse","109000","raton logitech",1)
+INSERT INTO productos(nombre,precio,descripcion,cantidad) values("monitor","80000","monitor gamer lg",1)
+
+INSERT INTO ventas(nombre_venta,fecha,cliente,producto) values("venta 1","21-11-2021","benjamin","teclado")
+INSERT INTO ventas(nombre_venta,fecha,cliente,producto) values("venta 2","21-11-2021","juan","mouse")
+INSERT INTO ventas(nombre_venta,fecha,cliente,producto) values("venta 3","21-11-2021","gabriel","monitor")
